87c622959c3c2685c4328159316128c9376aeb79
Enrique990 <voidenrique@gmail.com>
2024-09-23 08:38:48 -0600

    Clase Operacion creada

---
+++ Corte1/Corte1/models/Operacion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Operacion
+    {
+        public int CalcularEdad(Persona persona)
+        {
+            int yearActual = 2024; 
+            int monthActual = 9;  
+            int dayActual = 23;     
+
+            int edad = yearActual - persona.FechaDeNacimiento.Year;
+
+            if (persona.FechaDeNacimiento.Month > monthActual ||
+                (persona.FechaDeNacimiento.Month == monthActual && persona.FechaDeNacimiento.Day > dayActual))
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public bool EsMayorDeEdad(Persona persona)
+        {
+            int edad = CalcularEdad(persona);
+            return edad >= 18;
+        }
+    }
+}
